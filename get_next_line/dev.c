ae06dab04ef48d411412dc47dbd8cb0b1633dd71
田中　掌 <tanaka_tsukasa@tsukasa-no-MacBook-Pro.local>
2025-04-29 16:55:41 +0900

    norm以外完成！

--- get_next_line/dev.c
+++ get_next_line/dev.c
@@ -1,18 +1,11 @@
 #include "get_next_line.h"
-
-int main(void) {
-    int fd1;
-    int fd2;
-
-    fd1 = open("hungry.txt", O_RDONLY);
-    fd2 = open("full.txt", O_RDONLY);
-    printf("%s", get_next_line(fd1));
-    printf("%s", get_next_line(fd2));
-    printf("%s", get_next_line(fd1));
-    printf("%s", get_next_line(fd2));
-    printf("%s", get_next_line(fd1));
-    printf("%s", get_next_line(fd2));
-    close(fd1);
-    close(fd2);
-    return (0);
+int main()
+{
+    char *s2;
+    char *s4;
+    int fd;
+    fd = open("test.txt", O_RDONLY);
+    s2 = get_next_line(fd);
+    s4 = get_next_line(fd);
+    printf("%s%s", s2, s4);
 }
