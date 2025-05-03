6c74d74e06c03d1ed350dd3134c6c6746ee19487
Tsukasa Tanaka <ttanaka@c1r1s1.42tokyo.jp>
2025-05-03 14:17:20 +0900

    Linux0503

--- get_next_line/dev.c
+++ get_next_line/dev.c
@@ -1,11 +1,13 @@
 #include "get_next_line.h"
 int main()
 {
+    char *s1;
     char *s2;
-    char *s4;
+	char *s3;
     int fd;
-    fd = open("test.txt", O_RDONLY);
+    fd = open("1char.txt", O_RDONLY);
+    s1 = get_next_line(fd);
     s2 = get_next_line(fd);
-    s4 = get_next_line(fd);
-    printf("%s%s", s2, s4);
+	s3 = get_next_line(fd);
+    printf("%s%s%s", s1, s2, s3);
 }
