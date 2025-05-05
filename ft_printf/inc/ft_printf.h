bb4481024979d38f4232418249b8a2dbf4aafd49
Tsukasa Tanaka <ttanaka@c1r1s1.42tokyo.jp>
2025-05-05 18:10:12 +0900

    printfkannsei

--- ft_printf/inc/ft_printf.h
+++ ft_printf/inc/ft_printf.h
@@ -1,24 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttanaka <ttanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 12:35:48 by ttanaka           #+#    #+#             */
+/*   Updated: 2025/05/05 12:35:49 by ttanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-//utils
-int	ft_print_addr(void *ptr, int *cnt);
-int	ft_print_hex(unsigned int un, int islower, int *cnt);
-int f_ptr_parser(char format, void *arg);
-int f_int_parser(char format, int arg);
+// utils
+int		ft_print_addr(void *ptr, int *cnt);
+int		ft_print_hex(unsigned int un, int islower, int *cnt);
+int		f_ptr_parser(char format, void *arg);
+int		f_int_parser(char format, int arg);
 
-//libft_for_ft_printf
-int	ft_putchar(char c);
-int 	ft_putnbr_and_len(int n, int *cnt);
-int    	ft_putunbr_and_len(unsigned int un, int *cnt);
-int	ft_putstr_and_len(char *s);
+// libft_for_ft_printf
+int		ft_putchar(char c);
+int		ft_putnbr_and_len(int n, int *cnt);
+int		ft_putunbr_and_len(unsigned int un, int *cnt);
+int		ft_putstr_and_len(char *s);
 size_t	ft_strlen(const char *s);
 
-int     format_parser(va_list args, char format);
+int		format_parser(va_list args, char format);
 int		ft_printf(const char *format, ...);
 
 #endif
--- ft_printf/src/ft_printf_utils.c
+++ ft_printf/src/ft_printf_utils.c
@@ -1,92 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttanaka <ttanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 12:34:05 by ttanaka           #+#    #+#             */
+/*   Updated: 2025/05/05 12:34:06 by ttanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/ft_printf.h"
 
-int ft_print_addr(void *ptr, int *cnt)
+int	ft_print_addr(void *ptr, int *cnt)
 {
-	char	*hex;
-	char	buffer[16];
+	char			*hex;
+	char			buffer[16];
 	unsigned long	addr;
-	int 	i;
+	int				i;
 
+	if (!ptr)
+		return (-2);
 	hex = "0123456789abcdef";
 	addr = (unsigned long)ptr;
 	i = 15;
-
 	while (i >= 0)
 	{
 		buffer[i--] = hex[addr % 16];
 		addr /= 16;
 	}
-    i = 0;
-    while (buffer[i] == '0' && i < 15)
-        i++;
-    if (write(1, "0x", 2) == -1 || write(1, &buffer[i], 16 - i) == -1)
-        return (-1);
-    *cnt += (18 - i);
-    return (0);
+	i = 0;
+	while (buffer[i] == '0' && i++ < 15)
+		;
+	*cnt += (18 - i);
+	if (write(1, "0x", 2) == -1 || write(1, &buffer[i], 16 - i) == -1)
+		return (-1);
+	return (0);
 }
 
 int	ft_print_hex(unsigned int un, int islower, int *cnt)
 {
-    char c;
-    if (un > 15)
-        if (ft_print_hex(un / 16, islower, cnt) == -1)
-            return (-1);
-    if (islower)
-        c = "0123456789abcdef"[un % 16];
-    else
-        c = "0123456789ABCDEF"[un % 16];
-    (*cnt)++;
-    if (ft_putchar(c) == -1)
-        return (-1);
-    return (0);
+	char	c;
+
+	if (un > 15)
+		if (ft_print_hex(un / 16, islower, cnt) == -1)
+			return (-1);
+	if (islower)
+		c = "0123456789abcdef"[un % 16];
+	else
+		c = "0123456789ABCDEF"[un % 16];
+	(*cnt)++;
+	if (ft_putchar(c) == -1)
+		return (-1);
+	return (0);
 }
 
-int f_ptr_parser(char format, void *arg)
+int	f_ptr_parser(char format, void *arg)
 {
-    int cnt;
-    int n;
-    cnt = 0;
+	int	cnt;
+	int	n;
 
-    if (format == 's')
-    {
-        n = ft_putstr_and_len((char *)arg);
-        if (n == -1)
-            return (-1);
-        cnt += n;
-    }
-    else if (format == 'p')
-    {
-        if (ft_print_addr(arg, &cnt) == -1)
-            return (-1);
-    }
-    return (cnt);
+	cnt = 0;
+	if (format == 's')
+	{
+		n = ft_putstr_and_len((char *)arg);
+		if (n == -1)
+			return (-1);
+		cnt += n;
+	}
+	else if (format == 'p')
+	{
+		n = ft_print_addr(arg, &cnt);
+		if (n == -2)
+		{
+			if (write(1, "(nil)", 5) == -1)
+				return (-1);
+			cnt += 5;
+		}
+		if (n == -1)
+			return (-1);
+	}
+	return (cnt);
 }
 
-int f_int_parser(char format, int arg)
+int	f_int_parser(char format, int arg)
 {
-    int cnt;
+	int	cnt;
 
-    cnt = 0;
-    if (format == 'c')
-    {
-        if (ft_putchar((char)arg) == -1)
-            return (-1);
-        cnt++;
-    }
-    else if (format == 'u')
-    {
-        if (ft_putunbr_and_len((unsigned int)arg, &cnt) == -1)
-            return (-1);
-    }
-    else if (format == 'x' || format == 'X')
-    {
-        if (ft_print_hex((unsigned int)arg, (format == 'x'), &cnt) == -1)
-            return (-1);
-    }
-    else
-    {
-        if (ft_putnbr_and_len(arg, &cnt) == -1)
-            return (-1);
-    }
-    return (cnt);
+	cnt = 0;
+	if (format == 'c')
+	{
+		if (ft_putchar((char)arg) == -1)
+			return (-1);
+		cnt++;
+	}
+	else if (format == 'u')
+	{
+		if (ft_putunbr_and_len((unsigned int)arg, &cnt) == -1)
+			return (-1);
+	}
+	else if (format == 'x' || format == 'X')
+	{
+		if (ft_print_hex((unsigned int)arg, (format == 'x'), &cnt) == -1)
+			return (-1);
+	}
+	else
+	{
+		if (ft_putnbr_and_len(arg, &cnt) == -1)
+			return (-1);
+	}
+	return (cnt);
 }
