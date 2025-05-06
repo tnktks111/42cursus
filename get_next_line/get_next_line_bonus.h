eb281b86e2702a69d3d7732dbbcf04e86dede4cc
Tsukasa Tanaka <ttanaka@c6r1s2.42tokyo.jp>
2025-05-06 12:09:46 +0900

    gnl complete

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,69 +6,79 @@
 /*   By: ttanaka <ttanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 22:29:21 by ttanaka           #+#    #+#             */
-/*   Updated: 2025/05/05 23:57:29 by ttanaka          ###   ########.fr       */
+/*   Updated: 2025/05/06 11:59:20 by ttanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
-#define GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+// # include <fcntl.h>
 
-#define MALLOC_ERR -2
-#define RD_ERR -3
+# define MALLOC_ERR -2
+# define RD_ERR -3
 
-#ifndef BUFFER_SIZE
-    #define BUFFER_SIZE 512
-#endif
-
-# if BUFFER_SIZE > 8192 || BUFFER_SIZE < 1
-#  undef BUFFER_SIZE
-#  define BUFFER_SIZE 512
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
 # endif
 
-#define HASH_SIZE 1024
+# if BUFFER_SIZE < 1
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# define HASH_SIZE 1024
+
+# if BUFFER_SIZE >= 1000000
+#  undef HASH_SIZE
+#  define HASH_SIZE 1000
+# endif
+
+# if BUFFER_SIZE >= 10000000
+#  undef HASH_SIZE
+#  define HASH_SIZE 100
+# endif
 
 typedef struct s_string
 {
-    char *str;
-    size_t len;
-    size_t cap;
-} t_string;
+	char				*str;
+	size_t				len;
+	size_t				cap;
+}						t_string;
 
 typedef struct s_buf
 {
-    char buf[BUFFER_SIZE];
-    char *bufp;
-    int  bufsiz;
-} t_buf;
+	char				buf[BUFFER_SIZE];
+	char				*bufp;
+	int					bufsiz;
+}						t_buf;
 
 typedef struct s_buf_node
 {
-    int fd;
-    t_buf buffer;
-    struct s_buf_node *next;
-} t_buf_node;
+	int					fd;
+	t_buf				buffer;
+	struct s_buf_node	*next;
+}						t_buf_node;
 
 typedef struct s_hashtable
 {
-    t_buf_node  table[HASH_SIZE];
-    int         initialized;
-} t_hashtable;
+	t_buf_node			table[HASH_SIZE];
+	int					initialized;
+}						t_hashtable;
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-t_buf_node *create_buf_node(int fd);
-void init_buf_node(t_buf_node *node, int fd);
-char *adjust_string(t_string *res);
+void					ft_bzero(void *s, size_t n);
+void					*ft_memmove(void *dst, const void *src, size_t len);
+t_buf_node				*create_buf_node(int fd);
+void					init_buf_node(t_buf_node *node, int fd);
+char					*adjust_string(t_string *res);
 
-int ft_getc(t_buf_node *node);
-int ft_putc(t_string *str, char c);
-t_buf_node *ft_search(int fd, t_buf_node *hashtable);
-int ft_get_and_put_c(t_buf_node *node, t_string *res);
-char *get_next_line(int fd);
+int						ft_getc(t_buf_node *node);
+int						ft_putc(t_string *str, char c);
+t_buf_node				*ft_search(int fd, t_buf_node *hashtable);
+int						ft_get_and_put_c(t_buf_node *node, t_string *res);
+char					*get_next_line(int fd);
 
 #endif
